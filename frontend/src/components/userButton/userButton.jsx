1fede807adc30f7a7465bece4b5c0b98d53e5fc0
ltlylfun <funjee126@qq.com>
2025-04-21 19:32:02 +0800

    添加用户按钮组件及其交互逻辑

--- frontend/src/components/userButton/userButton.jsx
+++ frontend/src/components/userButton/userButton.jsx
@@ -1,6 +1,30 @@
 import "./userButton.css";
-
+import { useState } from "react";
 const UserButton = () => {
-  return <div className="userButton">UserButton</div>;
+  const [open, setOpen] = useState(false);
+  const curreentUser = true;
+
+  return curreentUser ? (
+    <div className="userButton">
+      <img src="/plipli/noAvatar.png" alt="" />
+      <img
+        onClick={() => setOpen((prev) => !prev)}
+        src="/plipli/arrow.svg"
+        alt=""
+        className="arrow"
+      />
+      {open && (
+        <div className="userOptions">
+          <div className="userOption">Profile</div>
+          <div className="userOption">Settings</div>
+          <div className="userOption">Logout</div>
+        </div>
+      )}
+    </div>
+  ) : (
+    <a href="" className="loginLink">
+      Login/Signup
+    </a>
+  );
 };
 export default UserButton;
